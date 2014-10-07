5ac03507058999541d9ec27113402c38029440b8
hqythu <hqythu@gmail.com>
2014-10-08 03:05:11 +0800

    update 2014/10/8 3 a.m.

---
+++ scripts/a9d61ca8.index.js
@@ -0,0 +1,1 @@
+"use strict";console.log("'Allo 'Allo!"),$(document).ready(function(){var a=$("#simplify").height();$(".bubble").height(a),$("#back").height($("#slogan").outerHeight()),$("#back-bottom").offset({top:$("body").height()-$("#footer").outerHeight()}),$("#back-bottom").height($("#footer").outerHeight())});
