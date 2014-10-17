7c9d06f66a5ad11d70fd9d4fe944e4f4bf32dbd4
hqythu <hqythu@gmail.com>
2014-10-17 23:40:42 +0800

    update 2014/10/17 11 p.m.

---
+++ scripts/2d084277.index.js
@@ -0,0 +1,1 @@
+"use strict";console.log("'Allo 'Allo!"),$(document).ready(function(){$("#back").height($("#slogan").outerHeight()),$("#back-bottom").offset({top:$("body").height()-$("#footer").outerHeight()}),$("#back-bottom").height($("#footer").outerHeight())});
--- scripts/a9d61ca8.index.js
+++
@@ -1,1 +0,0 @@
-"use strict";console.log("'Allo 'Allo!"),$(document).ready(function(){var a=$("#simplify").height();$(".bubble").height(a),$("#back").height($("#slogan").outerHeight()),$("#back-bottom").offset({top:$("body").height()-$("#footer").outerHeight()}),$("#back-bottom").height($("#footer").outerHeight())});
