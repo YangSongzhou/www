0a7736c881e1a1185014537d8a0906039ed7e6fc
hqythu <hqythu@gmail.com>
2015-03-11 14:48:03 +0800

    update 2015/3/11 7 p.m.

--- scripts/2d084277.index.js
+++
@@ -1,1 +0,0 @@
-"use strict";console.log("'Allo 'Allo!"),$(document).ready(function(){$("#back").height($("#slogan").outerHeight()),$("#back-bottom").offset({top:$("body").height()-$("#footer").outerHeight()}),$("#back-bottom").height($("#footer").outerHeight())});
